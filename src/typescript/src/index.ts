229d6a7df2fe2d730c15e49c51880f4f3ace7fe4
John Verbiest <john@johnverbiest.be>
2025-11-20 08:54:15 +0100

    Update .gitignore, index.ts, and package.json to include additional files and exports

--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -1,1 +1,11 @@
-﻿
+﻿// Main Validator and interface
+export { InszValidator } from './validators/InszValidator';
+export { IInszValidator } from './validators/IInszValidator';
+
+// Insz Number type
+export { InszNumber } from './types/InszNumber';
+
+// Supporting types
+export { InszValidationResult } from './types/InszValidationResult';
+export { ValidationError } from './types/ValidationError';
+export { Sex } from './types/Sex';
